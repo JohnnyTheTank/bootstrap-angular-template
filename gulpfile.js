313e73bbfeec053f9b33c1e474ccc807c8bd3cef
Jonathan Hornung <jonathan.hornung@gmail.com>
2016-09-03 21:08:26 +0200

    added node server

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 'use strict';
 
 var gulp        = require('gulp');
-var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
 var concat      = require('gulp-concat');
 var rename      = require('gulp-rename');
@@ -20,35 +19,20 @@ gulp.task('js-vendor', function(){
     return gulp.src(thirdPartyJavaScriptList)
         .pipe(sourcemaps.init())
         .pipe(concat('vendor.js'))
-        .pipe(gulp.dest('js'))
+        .pipe(gulp.dest('public/js'))
         .pipe(rename('vendor.min.js'))
         .pipe(uglify())
         .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest('js'));
-});
-
-// Static Server + watching scss/html files
-gulp.task('serve', [ 'create'], function() {
-    browserSync.init({
-        server: "./"
-    });
-
-    gulp.watch(["scss/*.scss"], ['sass']);
-    gulp.watch(thirdPartyJavaScriptList, ['js-vendor']);
-    gulp.watch([
-        "*.html",
-        "js/*.js"
-    ]).on('change', browserSync.reload);
+        .pipe(gulp.dest('public/js'));
 });
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
-    return gulp.src("scss/**/*.scss")
+    return gulp.src("public/scss/**/*.scss")
         .pipe(sass({outputStyle: 'compressed'}))
-        .pipe(gulp.dest("css"))
-        .pipe(browserSync.stream());
+        .pipe(gulp.dest("public/css"));
 });
 
 gulp.task('create', ['js-vendor', 'sass']);
 
-gulp.task('default', ['serve']);
+gulp.task('default', ['create']);
